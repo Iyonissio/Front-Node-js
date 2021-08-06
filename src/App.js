7fe37ad965fb9db8d1e89115cba227f4049a7369
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-08-06 14:37:23 +0200

    Routes add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/header";
+import Routes from './routes';
 
 function App() {
   return (
     <div>
       <Header />
-    </div>
+      <Routes />
+    </div>  
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import MainUsuario from './pages/usuario/main/index';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path = "/usuarios" component={MainUsuario} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Routes;
